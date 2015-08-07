e5b0603834b398f1a364546c0cd1fca8b988b1e9
Yecheng Fu <cofyc.jackson@gmail.com>
2015-08-07 10:23:30 +0800

    fix version gen script

--- gen-version.sh
+++ gen-version.sh
@@ -4,7 +4,7 @@
 version_file=version.h
 
 if test -d .git; then
-    version=$(git describe --always --match "v[0-9]*" | sed -e 's/-/./g' | sed -e 's/^v//g')
+    version=$(git describe --always --match "v[0-9]*" | sed -r -e 's/-([^-]+)$/.\1/g' | sed -e 's/^v//g')
     if test -f "$version_file"; then
         VC=$(perl -ne 'print "$1" if m/.*the_version = "(.*)".*/' $version_file)
     else
