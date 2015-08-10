0b2d22214853c061f5b74e1a4fadc7b98a195730
Yecheng Fu <cofyc.jackson@gmail.com>
2015-08-10 11:12:03 +0800

    Add aguments explictly.

--- example/start_wrapper.sh
+++ example/start_wrapper.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 ../dnscrypt-wrapper --resolver-address=114.114.114.114:53 --listen-address=0.0.0.0:8854 \
-    --provider-name=2.dnscrypt-cert.yechengfu.com -VV
+    --provider-name=2.dnscrypt-cert.yechengfu.com \
+    --provider-cert-file=dnscrypt.cert \
+    --crypt-secretkey-file=crypt_secret.key \
+    -VV
