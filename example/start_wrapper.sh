b89d4d20035f3e3bcd451770f5a89a62bdc23d96
Yecheng Fu <fuyecheng@qiniu.com>
2017-08-23 07:06:33 +0000

    docs: add TOC, etc

--- example/start_wrapper.sh
+++ example/start_wrapper.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ../dnscrypt-wrapper --resolver-address=8.8.8.8:53 --listen-address=0.0.0.0:8854 \
-    --provider-name=2.dnscrypt-cert.yechengfu.com \
+    --provider-name=2.dnscrypt-cert.example.com \
     --provider-cert-file=1.cert \
     --crypt-secretkey-file=1.key \
     -VV
