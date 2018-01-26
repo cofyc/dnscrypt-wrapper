b2954d69e3c44701d68d25c13e138d35c618441a
Frank Denis <github@pureftpd.org>
2018-01-26 15:48:36 +0100

    Include the signature in SignedCert

--- cert.h
+++ cert.h
@@ -14,13 +14,13 @@ struct SignedCert {
     uint8_t version_major[2];
     uint8_t version_minor[2];
 
+    uint8_t signature[crypto_sign_BYTES];
     // Signed Content
     uint8_t server_publickey[crypto_box_PUBLICKEYBYTES];
     uint8_t magic_query[8];
     uint8_t serial[4];
     uint8_t ts_begin[4];
     uint8_t ts_end[4];
-    uint8_t end[64];
 };
 
 struct Cert {
@@ -41,8 +41,6 @@ struct Cert {
 struct SignedCert *cert_build_cert(const uint8_t *crypt_publickey, int cert_file_expire_days, int use_xchacha20);
 int cert_sign(struct SignedCert *signed_cert,
               const uint8_t *provider_secretkey);
-int cert_unsign(struct SignedCert *signed_cert,
-                const uint8_t *provider_secretkey);
 void cert_display_txt_record_tinydns(struct SignedCert *signed_cert);
 void cert_display_txt_record(struct SignedCert *signed_cert);
 
